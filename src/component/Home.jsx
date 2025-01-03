a749e07cb3b020e1874177f4661c7c580fde3fcf
okechukwu david <david@koonage.com>
2025-01-03 06:21:13 +0100

    added pagination

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -28,6 +28,14 @@ const Home = () => {
     fetchData();
   }, []);
 
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   if (loading)
     return (
       <div className="flex justify-center items-center h-screen bg-white">
@@ -37,18 +45,41 @@ const Home = () => {
       </div>
     );
   return (
-    <div className="px-5 py-3 bg-gradient-to-l from-darkpurple to-darkblack h-screen flex justify-center items-center flex-col">
-      {data.map((item, index) => (
-        <div key={index} className="flex flex-row items-center gap-3">
-          <p className="font-roboto text-white text-start">{item.username}</p>
-          <button
-            onClick={() => navigate(`/user/${item.id}`)}
-            className="text-white w-full py-2 px-3 bg-transparent border-white border rounded-md"
-          >
-            View Details
-          </button>
+    <div className="px-5 py-5 bg-gradient-to-l from-darkpurple to-darkblack h-screen ">
+      <div className="w-full flex justify-between items-center  border-white border px-8 rounded-md ">
+        {" "}
+        <p className="text-white ">id</p>
+        <p className="text-white px-4">name</p>
+        <p className="text-white">Action</p>
+      </div>
+      {currentItems.map((item, index) => (
+        <div
+          key={index}
+          className="w-full flex justify-between items-center gap-10  px-8 "
+        >
+          <p className="text-white">{item.id}</p>
+          <p className="font-roboto text-white ">{item.username}</p>
+          <span className="pt-5">
+            <button
+              onClick={() => navigate(`/user/${item.id}`)}
+              className="text-white w-16 py-2 rounded-md bg-transparent border-white border font-playfair text-[10px]"
+            >
+              View Details
+            </button>
+          </span>
         </div>
       ))}
+      <div className="flex justify-center space-x-2 mt-20">
+        {[...Array(totalPages)].map((_, index) => (
+          <button
+            key={index}
+            onClick={() => paginate(index + 1)}
+            className="bg-transparent  text-white border-white border font-bold py-2 px-4 rounded"
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
